3ca0161f69e38f7c007d0bf9e00223bc89d891dd
camaj <joshua.g.camarena@gmail.com>
2024-01-19 16:43:18 -0800

    Update TunerConstants.java

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -94,7 +94,7 @@ public class TunerConstants {
         private static final int kFrontRightDriveMotorId = 17;
         private static final int kFrontRightSteerMotorId = 18;
         private static final int kFrontRightEncoderId = 22;
-        private static final double kFrontRightEncoderOffset = -0.341309;// -0.361328;
+        private static final double kFrontRightEncoderOffset = -0.169189;//-0.341309;// -0.361328;
                                                                          // //-0.340576;//-0.347656;//-0.35888671875;
 
         private static final double kFrontRightXPosInches = 10.375;
